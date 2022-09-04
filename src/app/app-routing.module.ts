3bdd93ebee5ac023e83505ed858c58bfe75231ca
Luillin Escobar <cataplum.17@hotmail.com>
2022-09-04 01:07:31 -0500

    desarrollo seccion sitio en construccion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainLayoutComponent} from "./modules/core/shared/components/main-layout/main-layout.component";
+import {ComingSoonComponent} from "./modules/core/shared/components/coming-soon/coming-soon.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'coming-soon',
+        pathMatch: "full"
+      },{
+      path: 'coming-soon',
+        component: ComingSoonComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './modules/core/shared/components/main-layout/main-layout.component';
+import { MainFooterComponent } from './modules/core/shared/components/main-footer/main-footer.component';
+import { MainNavbarComponent } from './modules/core/shared/components/main-navbar/main-navbar.component';
+import { ComingSoonComponent } from './modules/core/shared/components/coming-soon/coming-soon.component';
+import { NotificationFormComponent } from './modules/core/shared/components/notification-form/notification-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    MainFooterComponent,
+    MainNavbarComponent,
+    ComingSoonComponent,
+    NotificationFormComponent
   ],
   imports: [
     BrowserModule,
